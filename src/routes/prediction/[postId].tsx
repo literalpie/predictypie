db3721849948d0af53730cb1b7b7fc1ced574a49
Benjamin Kindle <benjaminkindle@yahoo.com>
2025-02-16 12:35:52 -0500

    typo

--- src/routes/prediction/[postId].tsx
+++ src/routes/prediction/[postId].tsx
@@ -2,7 +2,7 @@ export default function PredictionDetails () {
     return (
         <div>
         <h1>Prediction Details</h1>
-        TODO: Show the text of the prediciton, upvotes/downvotes, and other information.
+        TODO: Show the text of the prediction, upvotes/downvotes, and other information.
         </div>
     );
 }
