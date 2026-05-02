d03e643dc142e8a1f923a6b8e87751ad55307db5
Benjamin Kindle <20398475+literalpie@users.noreply.github.com>
2026-05-02 12:00:48 -0400

    add nitro back

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { solidStart } from "@solidjs/start/config";
+import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
 
 export default defineConfig({
-  plugins: [solidStart(), tailwindcss()],
+  plugins: [solidStart(), tailwindcss(), nitro({ preset: "netlify" })],
 });
