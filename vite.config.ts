a79538a815425cec312f45c678ed2bae70ab96a6
Benjamin Kindle <20398475+literalpie@users.noreply.github.com>
2026-05-02 10:44:11 -0400

    use vite 7
    vite 8 causes issues when building for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { solidStart } from "@solidjs/start/config";
 
 export default defineConfig({
-  plugins: [solidStart(), nitro(), tailwindcss()],
+  plugins: [solidStart(), tailwindcss()],
 });
