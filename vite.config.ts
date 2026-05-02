891875e33c10f7315f1e230649766628a648802c
Benjamin Kindle <20398475+literalpie@users.noreply.github.com>
2026-05-02 12:18:20 -0400

    remove preset

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { solidStart } from "@solidjs/start/config";
 import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
 
 export default defineConfig({
-  plugins: [solidStart(), tailwindcss(), nitro({ preset: "netlify" })],
+  plugins: [solidStart(), tailwindcss(), nitro()],
 });
